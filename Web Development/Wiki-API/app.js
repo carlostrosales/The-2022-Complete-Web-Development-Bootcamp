f907b4f4c62e7a893865f73a454a99f40622745c
carlostrosales <rosales.carlost@gmail.com>
2022-07-28 11:35:16 -0500

    Used the HTTP verb of GET to fetch all articles in local mongoDB database from the articles collection and rendered onto article route

---
+++ Web Development/Wiki-API/app.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const ejs = require("ejs");
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/wikiDB", { useNewUrlParser: true });
+
+const articleSchema = {
+  title: String,
+  content: String,
+};
+
+const Article = mongoose.model("Article", articleSchema);
+
+app.get("/articles", function (req, res) {
+  Article.find(function (err, foundArticles) {
+    if (!err) {
+      res.send(foundArticles);
+    }
+    res.send(err);
+  });
+});
+
+app.listen(3000, function () {
+  console.log("Server started succesfully on port 3000!");
+});
